86b680381dd47c34949efff4b8449585ce5c3139
Jay <psatdoctor@naver.com>
2019-06-19 21:08:46 +0900

    connected Mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://jdw:jdwjdwjdw@cluster0-ppakr.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const userRouter = require('./routers/api/users');
 const profileRouter = require('./routers/api/profile');
 const postsRouter = require('./routers/api/posts');
+
+//DB Config
+const db = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(db, {
+      useNewUrlParser: true, 
+      useCreateIndex: true  
+    }) 
+    //Adding new mongo url parser
+    .then(() => console.log('MongoDB connected..'))
+    .catch(err => console.log(err));
+
+
 
 const app = express();
 
