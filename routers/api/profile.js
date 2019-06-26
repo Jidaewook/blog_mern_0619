8a1e19c16f48fb43dc7fc29ebb97a6be2e23e0b0
Jay <psatdoctor@naver.com>
2019-06-26 20:15:25 +0900

    current profile route

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -1,13 +1,46 @@
 const express = require('express');
-const router = express.Router();
+const router = express();
 
-//@route GET api/profile/test
-//@desc Tests profile route
+const mongoose = require('mongoose');
+const userModel = require('../../models/user');
+const profileModel = require('../../models/profile');
+
+//private 접근을 할 때, accessToken을 넣기 위한 루트인즈
+const passport = require('passport');
+
+const authCheck = passport.authenticate('jwt', { session: false });
+
+
+//@router GET api/profile/tests
+//@desc tests Profile route
 //@access PUBLIC
 
+router.get('/test', (req, res) => res.json({ msg: 'Profile works' }));
+
+//@router GET api/profile
+//@desc get current user Profile route
+//@access Private
+
+router.get('/', (req, res) => {
+    const errors = {};
+
+    profileModel
+        .findOne({ user: req.body.id })
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors)
+            }
+            res.json(profile);
+        })
+        .catch(err => res.json(err))
+});
 
 
-router.get('/test', (req, res) => res.json({msg: 'Profile Works'}));
+//@router POST api/profile
+//@desc  Profile route
+//@access PUBLIC
+
 
 
 module.exports = router;
